5973031a33c9974b269f508fb6f32717c874f6a9
HikaruHirose <hiro_hika_1116@icloud.com>
2020-02-05 08:14:11 +0900

    tutrial-chapter5

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// タスク一覧ページ表示
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+// フォルダ作成ページの表示
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+
+// フォルダ作成処理の実行
+Route::post('folders/create', 'FolderController@create');
